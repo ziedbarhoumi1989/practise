8c18380abfcfe9ccbcb376a9dc44e4db33f814c9
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2020-05-11 12:13:57 +0100

    digital root solved

---
+++ problem1.js
@@ -0,0 +1,40 @@
+/*A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+
+Here's how it works:
+
+digital_root(16)
+=> 1 + 6
+=> 7
+
+digital_root(942)
+=> 9 + 4 + 2
+=> 15 ...
+=> 1 + 5
+=> 6
+
+digital_root(132189)
+=> 1 + 3 + 2 + 1 + 8 + 9
+=> 24 ...
+=> 2 + 4
+=> 6
+
+digital_root(493193)
+=> 4 + 9 + 3 + 1 + 9 + 3
+=> 29 ...
+=> 2 + 9
+=> 11 ...
+=> 1 + 1
+=> 2
+*/
+function digital_root(n) {
+  // ...
+  if(n <= 9) {
+    return  n
+  }
+  var numberToString = n.toString()
+  var result = 0;
+  for(var i=0;i<numberToString.length;i++) {
+  result += Number(numberToString[i])
+  }
+  return digital_root(result)
+}
