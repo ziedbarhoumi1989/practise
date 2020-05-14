8cc602c9aca1e89e02ee822331c6542e498d1469
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2020-05-14 12:56:51 +0100

    string permutations

---
+++ StringPermutations.js
@@ -0,0 +1,28 @@
+/*
+ you have to create all permutations of an input string and remove duplicates, if present. This means, you have to shuffle all letters from the input in all possible orders.
+
+Examples:
+
+permutations('a'); // ['a']
+permutations('ab'); // ['ab', 'ba']
+permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+The order of the permutations doesn't matter.
+*/
+
+
+
+
+/// SOLUTION///////////
+function permutations(string) {
+  var arr = string.split(''), tmp = arr.slice(), heads = [], out = [];
+  if(string.length == 1) return [string];
+  arr.forEach(function(v, i, arr) {
+    if(heads.indexOf(v) == -1) {
+      heads.push(v);
+      tmp.splice(tmp.indexOf(v), 1);
+      permutations(tmp.join('')).forEach(function(w) {out.push(v + w);});
+      tmp.push(v);
+    }
+  });
+  return out;
+}
