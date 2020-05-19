55bf673be496e72db27ea8179c24f86e8bad9a68
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2020-05-19 11:29:43 +0100

    multipling big int

---
+++ multiply numbers.js
@@ -0,0 +1,55 @@
+/*
+ Multiply two numbers! Simple!
+
+The arguments are passed as strings.
+The numbers may be way very large
+Answer should be returned as a string
+The returned "number" should not start with zeros e.g. 0123 is invalid
+Note: 100 randomly generated tests!
+*/
+
+
+
+/////// SOLUTION  ///////
+
+function multiply(a, b) {
+    if (a.length < 5 && b.length < 5) {
+        return (parseInt(a) * parseInt(b)).toString();
+    }
+
+    a = a.split('').reverse();
+    b = b.split('').reverse();
+    var result = [];
+
+    for (var i = 0; a[i] >= 0; i++) {
+        for (var j = 0; b[j] >= 0; j++) {
+            if (!result[i + j]) {
+                result[i + j] = 0;
+            }
+
+            result[i + j] += a[i] * b[j];
+        }
+    }
+
+    for (i = 0; result[i] >= 0; i++) {
+        if (result[i] >= 10) {
+            if (!result[i + 1]) {
+                result[i + 1] = 0;
+            }
+
+            result[i + 1] += parseInt(result[i] / 10);
+            result[i] %= 10;
+        }
+    }
+
+    result.reverse();
+    for (i = 0; i < result.length; i++){
+      if (result[i] === 0){
+         result.splice(i--, 1);
+      }else{
+        i = result.length * 2;
+      }
+    }
+
+    return result.join('');
+}
